1113fb53ccf49ce1ef8b6318c782a43bdc27f4c3
lopnao <nao.return@gmail.com>
2024-05-01 10:20:36 +0200

    pass 6th stage - module added and bunch

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+mod resp;
+
+use std::error::Error;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
+use crate::resp::Value;
 
 #[tokio::main]
 async fn main() {
@@ -22,5 +26,57 @@ async fn main() {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+async fn handle_conn(stream: TcpStream) {
+    let mut handler = resp::RespHandler::new(stream);
+
+    println!("Starting read loop");
+
+    loop {
+        let value = handler.read_value().await.unwrap();
+        println!("Got value : {:?}", value);
+
+        let response = if let Some(v) = value {
+            let (command, args) = extract_command(v).unwrap();
+            match command.as_str() {
+                "ping" => {
+                    Value::SimpleString("PONG".to_string())
+                }
+                "echo" => {
+                    args.first().unwrap().clone()
+                }
+                c => {
+                    panic!("Cannot handle this command : {:?}", c)
+                }
+            }
+        } else {
+            break;
+        };
+        println!("Sendind value: {:?}", response);
+
+        handler.write_value(response).await.unwrap()
+    }
+}
+
+fn extract_command(value: Value) -> Result<(String, Vec<Value>), anyhow::Error> {
+    match value {
+        Value::SimpleString(s) => {
+            Ok((s, vec![]))
+        }
+        Value::BulkString(s) => {
+            Ok((s, vec![]))
+        }
+        Value::Array(a) => {
+            Ok((unpack_bulk_str(a.first().unwrap().clone())?, a.into_iter().skip(1).collect()))
+        }
+    }
+}
+
+fn unpack_bulk_str(value: Value) -> Result<String, anyhow::Error> {
+    match value {
+        Value::BulkString(s) => Ok(s),
+        _ => Err(anyhow::anyhow!("Expected command to be a bulk string"))
     }
 }
