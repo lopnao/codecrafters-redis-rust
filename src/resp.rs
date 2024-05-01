49f4e6f33756cb4e126d57f0bc6a7300d42943cb
lopnao <nao.return@gmail.com>
2024-05-01 10:46:45 +0200

    pass 6th stage - module added and bunch

--- src/resp.rs
+++ src/resp.rs
@@ -167,21 +167,14 @@ fn parse_bulk_string(buffer: BytesMut) -> Result<(Value, usize)> {
 fn read_until_crlf(buffer: &[u8]) -> Option<(&[u8], usize)> {
 
     for i in 1..buffer.len() {
-
         if buffer[i - 1] == b'\r' && buffer[i] == b'\n' {
-
             return Some((&buffer[0..(i - 1)], i + 1));
-
         }
-
     }
 
-    return None;
-
+    None
 }
 
 fn parse_int(buffer: &[u8]) -> Result<i64> {
-
     Ok(String::from_utf8(buffer.to_vec())?.parse::<i64>()?)
-
 }
