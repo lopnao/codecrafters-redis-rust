0e4f116cb15f898d5c13902d2a2191791c41e0b2
lopnao <nao.return@gmail.com>
2024-05-01 09:36:06 +0200

    pass 3rd stage

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ fn main() {
             Ok(mut stream) => {
                 println!("accepted new connection");
                 let mut buf = [0; 512];
-                stream.read_exact(&mut buf).unwrap();
-                stream.write_all(b"+PONG\r\n").unwrap();
+                stream.read(&mut buf).unwrap();
+                stream.write(b"+PONG\r\n").unwrap();
             }
 
             Err(e) => {
