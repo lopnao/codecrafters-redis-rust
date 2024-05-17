35fcc88d71bd03d178caa621b99fed34f8811b0e
nao <nao@nao.nao>
2024-05-18 01:01:53 +0200

    pass stage :  Send handshake (2/3) ?

--- src/connect.rs
+++ src/connect.rs
@@ -1,5 +1,6 @@
 use tokio::net::TcpStream;
-use crate::SlaveError;
+use crate::resp::Value;
+use crate::{SlaveError, unpack_bulk_str};
 use crate::SlaveError::NoHost;
 
 pub async fn connect_to_master(master_host: Option<String>, master_port: Option<u16>) -> Result<TcpStream, SlaveError> {
@@ -11,4 +12,18 @@ pub async fn connect_to_master(master_host: Option<String>, master_port: Option<
     let mut stream = TcpStream::connect(format!("{}:{}", master_host.unwrap(), port)).await?;
 
     Ok(stream)
+}
+
+
+pub fn configure_replica(args: Vec<Value>) -> Value {
+    if args[0] == Value::BulkString("listening-port".to_string()) {
+        let slave_port = unpack_bulk_str(args[1].clone()).unwrap().parse::<u16>().unwrap();
+        println!("Received slave port of : {:?}", slave_port);
+
+        return Value::SimpleString("OK".to_string());
+    } else if   args[0] == Value::BulkString("capa".to_string()) &&
+                args[1] == Value::BulkString("psync2".to_string()) {
+        return Value::SimpleString("OK".to_string());
+    }
+    Value::SimpleString("NOK".to_string())
 }
