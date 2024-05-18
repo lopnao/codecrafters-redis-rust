a068cd0afc9a45666cdaf658e558042efeb54c09
nao <nao@nao.nao>
2024-05-18 02:29:20 +0200

    pass stage :  Send handshake (3/3) ?

--- src/db.rs
+++ src/db.rs
@@ -42,24 +42,30 @@ pub fn data_get(args: Vec<Value>, data1: Arc<Mutex<HashMap<String, KeyValueData>
     } else { Value::SimpleString("ERROR".to_string()) }
 }
 
-pub fn server_info(server_info_clone: Arc<RedisServer>, args: Vec<Value>) -> Value {
+pub fn server_info(server_info_clone: Arc<Mutex<RedisServer>>, args: Vec<Value>) -> Value {
     let mut res = vec![];
     let args: Vec<String> = args.iter().map(|arg| unpack_bulk_str(arg.clone()).unwrap()).collect();
     for arg in args {
         match arg.as_str() {
             "replication" => {
-                let is_master = server_info_clone.is_master;
-                let role = if server_info_clone.is_master { "master" } else { "slave" };
+                let is_master = { server_info_clone.lock().unwrap().is_master };
+                let role = if is_master { "master" } else { "slave" };
                 res.push(Value::BulkString("# Replication".to_string()));
                 res.push(Value::BulkString(format!("role:{}", role)));
                 if is_master {
-                    res.push(Value::BulkString(format!("connected_slaves:{}", server_info_clone.connected_slaves)));
+                    let connected_slaves = { server_info_clone.lock().unwrap().connected_slaves };
+                    res.push(Value::BulkString(format!("connected_slaves:{}", connected_slaves)));
                 }
-                res.push(Value::BulkString(format!("master_replid:{}", server_info_clone.master_replid)));
-                res.push(Value::BulkString(format!("master_repl_offset:{}", server_info_clone.master_repl_offset)));
-                res.push(Value::BulkString(format!("master_nanoid:{}", server_info_clone.master_nanoid)));
+                let master_replid = { server_info_clone.lock().unwrap().master_replid };
+                let master_repl_offset = { server_info_clone.lock().unwrap().master_repl_offset };
+                let master_nanoid = { server_info_clone.lock().unwrap().master_nanoid.clone() };
+                res.push(Value::BulkString(format!("master_replid:{}", master_replid)));
+                res.push(Value::BulkString(format!("master_repl_offset:{}", master_repl_offset)));
+                res.push(Value::BulkString(format!("master_nanoid:{}", master_nanoid)));
                 if !is_master {
-                    if let (Some(master_host), Some(master_port)) = (&server_info_clone.master_host, &server_info_clone.master_port) {
+                    let master_host = { server_info_clone.lock().unwrap().master_host.clone() };
+                    let master_port = { server_info_clone.lock().unwrap().master_port.clone() };
+                    if let (Some(master_host), Some(master_port)) = (master_host, master_port) {
                         res.push(Value::BulkString(format!("master_host:{}", master_host)));
                         res.push(Value::BulkString(format!("master_port:{}", master_port)));
                     }
